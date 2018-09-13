73f7648fb7d94c39631149f2ef1c148fb47bcad6
GuanXingWu <xeaiow@gmail.com>
2018-09-14 00:31:17 +0800

    * 取消 RWD

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,7 +3,7 @@
     <head>
         <title>CYIM Rent - @yield('title')</title>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=0">
         @section('style')
             <link rel="stylesheet" href="{{ asset('css/style.css') }}">
             <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
